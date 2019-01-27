a68ee2306f622d74334e325ebec4672f408778ab
carcigenicate <slomifier@gmail.com>
2019-01-27 13:08:08 -0700

    Made each progress bar text slightly offset to reduce overlapping

--- src/mandelbrot/seesaw/multi_progress_bar.clj
+++ src/mandelbrot/seesaw/multi_progress_bar.clj
@@ -5,8 +5,11 @@
             [seesaw.graphics :as sg]))
 
 (def prog-text-height-perc 0.15)
-(def prog-text-x-offset-perc 0.05)
-(def prog-text-y-offset-perc 0.2)
+(def prog-text-x-offset-perc 0.01)
+
+(def line-width 10)
+
+; TODO: Allow line width, color, etc to be set
 
 (defn clamp-n [n min-n max-n]
   (-> n
@@ -21,7 +24,7 @@
 
         [cw ch] (sh/component-dimensions canvas)
 
-        font {:size (int (* ch prog-text-height-perc))}]
+        font-size (int (* ch prog-text-height-perc))]
 
     (doseq [[id {:keys [current-progress max-progress]}] state-map
 
@@ -29,15 +32,16 @@
                   x (* cw prog-perc)
 
                   text-x (+ x (* cw prog-text-x-offset-perc))
-                  text-y (* ch prog-text-y-offset-perc)
+                  text-y (+ font-size
+                            (rem (* id font-size) (- ch font-size)))
                   desc-str (str id ": " (* prog-perc 100) " %")]]
 
       (sg/draw g
          (sg/line x 0, x ch)
-         (sg/style :foreground :black, :stroke 10)
+         (sg/style :foreground :black, :stroke line-width)
 
          (sg/string-shape text-x text-y desc-str)
-         (sg/style :font font)))))
+         (sg/style :font {:size font-size})))))
 
 (defn new-multi-progress-bar []
   (let [progress-state-atom (atom {})
@@ -50,23 +54,23 @@
 
     prog-bar))
 
-(defn- update-state [prog-bar f & args]
+(defn- update-state! [prog-bar f & args]
   (let [state-atom (sc/user-data prog-bar)]
     (apply swap! state-atom f args)))
 
 (defn add-job! [prog-bar id max-progress]
-  (update-state prog-bar
-    assoc id (new-progress-state max-progress)))
+  (update-state! prog-bar
+                 assoc id (new-progress-state max-progress)))
 
 (defn set-progress! [prog-bar id new-progress]
-  (update-state prog-bar
-    #(assoc-in % [id :current-progress]
-               (clamp-n new-progress 0 (get-in % [id :max-progress])))))
+  (update-state! prog-bar
+                 #(assoc-in % [id :current-progress]
+                            (clamp-n new-progress 0 (get-in % [id :max-progress])))))
 
 (defn increment-progress! [prog-bar id progress-to-add]
-  (update-state prog-bar
-    #(update-in % [id :current-progress]
-       (fn [p] (clamp-n (+ p progress-to-add) 0 (get-in % [id :max-progress]))))))
+  (update-state! prog-bar
+                 #(update-in % [id :current-progress]
+                    (fn [p] (clamp-n (+ p progress-to-add) 0 (get-in % [id :max-progress]))))))
 
 (defn new-test-frame []
   (let [bar (new-multi-progress-bar)
